4a9710653c946b048232eb46f95bea3c17d47445
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-02-05 16:15:18 -0300

    feat: components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { ShoopingListEditComponent } from './shooping-list-edit/shooping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoopingListEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shooping-list-edit/shooping-list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shooping-list-edit',
+  templateUrl: './shooping-list-edit.component.html',
+  styleUrls: ['./shooping-list-edit.component.css']
+})
+export class ShoopingListEditComponent {
+
+}
